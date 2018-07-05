3e22405b9c90f9015f31a3cedb696bfe8afe8fb7
taf <taffik@yandex.ru>
2018-07-05 15:32:06 +0300

    add edit time and date

---
+++ src/components/Meetup/Edit/EditMeetupDateDialog.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-dialog width="350px" persistent v-model="editDialog">
+
+        <v-btn accent slot="activator">
+            Edit date
+        </v-btn>
+        <v-card>
+            <v-container>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-card-title>Edit Meetup Date</v-card-title>
+                    </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-date-picker
+                            v-model="editableDate" 
+                            style="width: 100%" 
+                            actions
+                            reactive                            
+                            >
+                            <template slot-scope="{save, cancel}">
+                                <v-btn class="blue--text darken-1"
+                                        flat
+                                        @click.native="editDialog = false">
+                                        Close
+                                </v-btn>
+                                <v-btn class="blue--text darken-1"
+                                        flat
+                                        @click.native="onSaveChanges()">
+                                        Save
+                                </v-btn>
+                            </template>
+                        </v-date-picker>
+                    </v-flex>
+                </v-layout>
+                
+            </v-container>
+        </v-card>
+
+
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props: ['meetup'],
+    data() {
+        return{
+            editDialog: false,
+            editableDate: null,
+        }
+    }, 
+    methods: {
+        onSaveChanges() {
+            let newDate = new Date(this.meetup.date)
+            newDate.setUTCDate(new Date(this.editableDate).getUTCDate())
+            newDate.setUTCMonth(new Date(this.editableDate).getUTCMonth())
+            newDate.setUTCFullYear(new Date(this.editableDate).getUTCFullYear())
+            
+            this.$store.dispatch('updateMeetupData', {
+                id: this.meetup.id,
+                date: newDate
+            })
+        }            
+    },
+    created() {
+        const pickerDate = new Date(this.meetup.date)
+        this.editableDate = pickerDate.getUTCFullYear () + '-' + (pickerDate.getUTCMonth () + 1) + '-' + pickerDate.getUTCDate ()
+    }
+    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import AlertCmp from './components/Shared/Alert'
 import 'vuetify/dist/vuetify.min.css'
 import DateFilter from './filters/date'
 import EditMeetpDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog'
+import EditMeetpDateDialog from './components/Meetup/Edit/EditMeetupDateDialog'
+import EditMeetpTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog'
 
 Vue.use(Vuetify, { theme: {
   primary: '#e3aa8a',
@@ -27,6 +29,8 @@ Vue.config.productionTip = false
 Vue.filter('date', DateFilter)
 Vue.component('app-alert', AlertCmp)
 Vue.component('app-edit-meetup-details-dialog', EditMeetpDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetpDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetpTimeDialog)
 
 /* eslint-disable no-new */
 new Vue({
